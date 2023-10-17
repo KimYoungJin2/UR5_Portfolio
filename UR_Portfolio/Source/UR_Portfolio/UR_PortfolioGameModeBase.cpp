450c5d2fc154787d4943d2547093f6a044716aee
니나니모 <carl2005@naver.com>
2023-10-17 15:09:49 +0900

    첫세팅

---
+++ UR_Portfolio/Source/UR_Portfolio/UR_PortfolioGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "UR_PortfolioGameModeBase.h"
+
---
+++ UR_Portfolio/Source/UR_Portfolio/UR_PortfolioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UR_PortfolioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UR_PORTFOLIO_API AUR_PortfolioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
